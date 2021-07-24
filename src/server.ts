92102642bfba04d38f74d49a704ff3b0d5246497
Ashkan Ashtiani <ashcan@3dln.com>
2021-07-24 09:11:40 +0430

    fixed print size

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,4 @@
 // node.js
-import fs from "fs";
 import path from "path";
 import express from "express";
 
@@ -17,31 +16,26 @@ import { printSavedFile } from "./helpers/printer";
 
 // Constants
 const PORT = 5000;
-const HOST = '127.0.0.1';
+const HOST = "0.0.0.0";
 const app = express();
 
 app.use(express.json());
-// var corsOptions = {
-//     origin: '*',
-//     optionsSuccessStatus: 200,
-// };
-// app.use(cors(corsOptions));
-app.use((req, res, next) => {
-    const origin = req.get('origin');
+var corsOptions = {
+    allowedHeaders: [
+        "Origin",
+        "X-Requested-With",
+        "Content-Type",
+        "Accept",
+        "X-Access-Token",
+    ],
+    credentials: true,
+    methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+    origin: "*",
+    preflightContinue: false,
+};
 
-    // TODO Add origin validation
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods', 'GET, POST');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control, Pragma');
+app.use(cors(corsOptions));
 
-    // intercept OPTIONS method
-    if (req.method === 'OPTIONS') {
-        res.sendStatus(204);
-    } else {
-        next();
-    }
-});
 app.use(morgan("dev"));
 app.use(trim);
 app.get("/getprinters", (_, res) => {
@@ -57,35 +51,48 @@ app.post("/print", async (req, res) => {
     const filePath = path.join(__dirname, "bills", `${orderId}.pdf`);
 
     html_to_pdf
-        .generatePdf({ url: billUrl }, { format: "A4" })
-        .then((pdfBuffer: Buffer) => {
-            fs.writeFile(filePath, pdfBuffer, (err) => {
-                if (err) {
+        .generatePdf(
+            { url: billUrl },
+            {
+                format: "A5",
+                scale: 2,
+                margin: {
+                    top: "1cm",
+                    bottom: "1cm",
+                    left: "1cm",
+                    right: "1cm",
+                },
+
+                path: filePath,
+            }
+        )
+        .then(() => {
+            // return res.json({ success: true });
+            // fs.writeFile(filePath, pdfBuffer, (err) => {
+            //     if (err) {
+            //         return res.status(500).json({ success: false });
+            //     }
+            printSavedFile(printers, filePath)
+                .then(() => {
+                    return res.json({ success: true });
+                })
+                .catch(() => {
                     return res.status(500).json({ success: false });
-                }
-                printSavedFile(printers, filePath)
-                    .then(() => {
-                        return res.json({ success: true });
-                    })
-                    .catch(() => {
-                        return res.status(500).json({ success: false });
-                    });
-            });
+                });
         });
 });
 
 app.use("/api/auth", authRoutes);
 
-
 app.listen(PORT, HOST, async () => {
     console.log(`Server started at ${HOST} on port ${PORT}`);
-});;
+});
 // app.listen(5000, async () => {
 //     console.log(`Server started`);
-    // try {
-    //     await createConnection();
-    //     console.log(`Database connected`);
-    // } catch (err) {
-    //     console.log(err);
-    // }
+// try {
+//     await createConnection();
+//     console.log(`Database connected`);
+// } catch (err) {
+//     console.log(err);
+// }
 // });
